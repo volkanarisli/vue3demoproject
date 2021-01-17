f5be6ae39578527e908b5fa97c3a378489d11d08
Volkan <volkanarisli@gmail.com>
2021-01-17 17:59:45 +0300

    UserProfile component added

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <user-profile msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserProfile from "./components/UserProfile.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    UserProfile,
+  },
+};
 </script>
 
 <style>
@@ -19,8 +18,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  min-height: 100vh;
+  background-color: #f3f5fa;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="user-profile">
+    <div class="user-profile__sidebar">
+      <div class="user-profile__user-panel">
+        <h1 class="user-profile__username">@{{ user.username }}</h1>
+        <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
+        <div class="user-profile__follower-count">
+          <strong>Followers: </strong> {{ followers }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      followers: 0,
+      user: {
+        id: 1,
+        username: "_deneme",
+        firstName: "foo",
+        lastName: "bar",
+        email: "foo@gmail.com",
+        isAdmin: true,
+      },
+    };
+  },
+  watch: {
+    followers(newVal, oldVal) {
+      if (oldVal < newVal)
+        console.log(`${this.user.username} has gained a follower!`);
+    },
+  },
+  computed: {
+    fullName() {
+      return `${this.user.firstName} ${this.user.lastName}`;
+    },
+  },
+  methods: {
+    followUser() {
+      this.followers++;
+    },
+  },
+  mounted() {
+    this.followUser();
+  },
+};
+</script>
+
+<style>
+.user-profile {
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-gap: 50px;
+  padding: 50px 5%;
+}
+.user-profile__user-panel {
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+  margin-bottom: auto;
+}
+.user-profile__admin-badge {
+  background: rebeccapurple;
+  color: white;
+  border-radius: 5px;
+  margin-right: auto;
+  padding: 0 10px;
+  font-weight: bold;
+}
+h1 {
+  margin: 0;
+}
+</style>
