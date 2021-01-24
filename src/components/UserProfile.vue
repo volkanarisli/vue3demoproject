db1a51128695e6125217fd1492f748350575021b
Volkan <volkanarisli@gmail.com>
2021-01-24 10:19:14 +0300

    Twoot Form added

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,22 +1,61 @@
 <template>
   <div class="user-profile">
-    <div class="user-profile__sidebar">
+    <div>
       <div class="user-profile__user-panel">
         <h1 class="user-profile__username">@{{ user.username }}</h1>
         <div class="user-profile__admin-badge" v-if="user.isAdmin">Admin</div>
         <div class="user-profile__follower-count">
           <strong>Followers: </strong> {{ followers }}
         </div>
+        <form
+          class="user-profile__create-twoot"
+          @submit.prevent="createNewTwoot"
+        >
+          <label for="newTwoot"><strong>New Twoot</strong></label>
+          <textarea v-model="newTwootContent" id="newTwoot" rows="4" />
+          <div class="user-profile_create-twoot-type">
+            <label for="newTwootType"><strong>Type: </strong></label>
+            <select id="newTwootType" v-model="selectedTwootType">
+              <option
+                v-for="(option, index) in twootTypes"
+                :value="option.value"
+                :key="index"
+              >
+                {{ option.name }}
+              </option>
+            </select>
+          </div>
+          <button>Twoot!</button>
+        </form>
       </div>
+    </div>
+    <div class="user-profile__twoots-wrapper">
+      <twoot-item
+        v-for="twoot in user.twoots"
+        :key="twoot.id"
+        :twoot="twoot"
+        :username="user.username"
+        @favoriteTwoot="toggleFavorite"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import TwootItem from "./TwootItem.vue";
 export default {
   name: "App",
+  components: {
+    TwootItem,
+  },
   data() {
     return {
+      newTwootContent: "",
+      selectedTwootType: "instant",
+      twootTypes: [
+        { value: "draft", name: "Draft" },
+        { value: "instant", name: "Instant" },
+      ],
       followers: 0,
       user: {
         id: 1,
@@ -25,6 +64,10 @@ export default {
         lastName: "bar",
         email: "foo@gmail.com",
         isAdmin: true,
+        twoots: [
+          { id: 1, content: "Twotter is amazing!!" },
+          { id: 2, content: "Hello world" },
+        ],
       },
     };
   },
@@ -42,6 +85,18 @@ export default {
   methods: {
     followUser() {
       this.followers++;
+    },
+    toggleFavorite(id) {
+      console.log(id);
+    },
+    createNewTwoot() {
+      if (this.newTwootContent && this.selectedTwootType !== "draft") {
+        this.user.twoots.unshift({
+          id: this.user.twoots.length + 1,
+          content: this.newTwootContent,
+        });
+        this.newTwootContent = "";
+      }
     },
   },
   mounted() {
@@ -73,8 +128,28 @@ export default {
   margin-right: auto;
   padding: 0 10px;
   font-weight: bold;
+  margin-bottom: 20px;
+}
+.user-profile__twoots-wrapper {
+  display: grid;
+  grid-gap: 10px;
+  margin-bottom: auto;
+}
+.twoot-item {
+  padding: 20px;
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid #dfe3e8;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.25s ease;
 }
 h1 {
   margin: 0;
+}
+.user-profile__create-twoot {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
 }
 </style>
